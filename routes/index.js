1b0f6eefe894d1ac577fe35dfb3f1f83b76bcecb
Taufik <taufik9890@gmail.com>
2024-02-16 18:51:33 +0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const apiRoute = require('./api')
+const apiRoute = require('./api/index')
 
 
 route.use(process.env.API_URL, apiRoute)
