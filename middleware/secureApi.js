ea3d70c30dcd9e2d2168ba1fbb9deb1c5214d039
Taufik <taufik9890@gmail.com>
2024-02-17 17:44:39 +0600

    n

--- middleware/secureApi.js
+++ middleware/secureApi.js
@@ -1,6 +1,11 @@
 const secureApi =(req, res, next)=>{
-    console.log('hoise');
-    next()
+    console.log('hoise', req.Headers);
+    if(req.Headers.authorization == "3sw20aYo'?Nq"){
+        next()
+    }
+    else{
+        res.send({error:'invalid api'})
+    }
 }
 module.exports = secureApi
 // ekhane next() er kaaj hocche permission pawar pore jaite deya ke bole 
